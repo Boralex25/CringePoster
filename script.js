8bbade0fff9915f1e6eb8a5f23726845df039885
Lexx Bor <alex2007bor@gmail.com>
2026-01-27 18:41:16 +0300

    add_snake

--- script.js
+++ script.js
@@ -27,69 +27,34 @@ document.addEventListener('DOMContentLoaded', () => {
         let rect = snake.getBoundingClientRect()
         let x = rect.left
         let y = rect.top
-        let delX = 2
+        let delX = 20
         let delY = 0
         let isMoving = false
 
-        let trail = []
-        let maxLengthTrail = 15
-        let tailDots = []
-
-        
-
-        function render() {
-            for (let i = 0; i < maxLengthTrail; i++) {
-            console.log('yes')
-            let dot = document.createElement('div')
-            dot.style.position = 'absolute'
-            let size = snake.getBoundingClientRect().width
-            dot.style.width = `${size}px`
-            dot.style.height = `${size}px`
-            dot.style.backgroundImage = 'url(assets/face.svg)'
-            dot.style.backgroundSize = 'contain'
-            dot.style.backgroundRepeat = 'no-repeat'
-            dot.style.pointerEvents = 'none'
-            dot.style.opacity = 1 - i / maxLengthTrail
-            dot.style.zIndex = '-1'
-            dot.style.display = 'none'
-            document.body.appendChild(dot)
-            tailDots.push(dot)
+        let canSpawn = true
+        function spawnTail(x, y){
+            if(!canSpawn) return
+            canSpawn = false
+            let tail = new Image()
+            tail.src = "./assets/face.svg"
+            tail.style.height = "12%"
+            tail.style.left = `${x}px`
+            tail.style.top = `${y}px`
+            snake.style.opacity = 0
+            document.body.appendChild(tail)
+            setTimeout(() => { canSpawn = true }, 420)
+            setTimeout(() => tail.remove(), 2000)
         }
+        function render() {
             snake.style.left = `${x}px`
             snake.style.top = `${y}px`
-
-            /*tailDots.forEach(dot => dot.style.display = 'none')*/
-
-            trail.forEach((pos, i) => {
-                let dot = tailDots[i]
-                if (!dot) return
-                dot.style.display = 'block'
-                dot.style.left = `${pos.x - segmentDistance / 2}px`
-                dot.style.top = `${pos.y - segmentDistance / 2}px`
-
-            })
         }
-
-
-        let segmentDistance = 40
-
         function update(){
             if(isMoving){
                 console.log('update');
-                
                 x += delX
                 y += delY
-                if(trail.length === 0){
-                    trail.push({ x, y })
-                }
-                if (Math.hypot(x - trail[trail.length - 1].x, y - trail[trail.length - 1].y) >= segmentDistance) 
-                {
-                    trail.push({ x, y })
-                }
-                if (trail.length > maxLengthTrail)
-                {
-                    trail.shift()
-                }
+                spawnTail(x, y)
             }
             render()
             requestAnimationFrame(update)
